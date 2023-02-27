e6146b5562fba18cf90eee7973881017d113037b
manle12 <126459513+manle12@users.noreply.github.com>
2023-02-27 01:40:27 -0800

    Update client.example.js

--- constants/credentials/client.example.js
+++ constants/credentials/client.example.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: '',
-  authDomain: '',
+  apiKey: 'AIzaSyDKvi5PiaK5zB-Um_iJrQQyqb8Lh46Q728',
+  authDomain: 'brobet-master.firebaseapp.com',
   databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  projectId: 'brobet-master',
+  storageBucket: 'brobet-master.appspot.com',
+  messagingSenderId: '810579615254'
 }
